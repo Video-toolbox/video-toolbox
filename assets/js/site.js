d72239a266a55be9fa6ef69384167beb29a074e7
bo-nicolaisen <bo@squirrelworks.dk>
2024-05-13 23:01:24 +0200

    navigation

--- assets/js/site.js
+++ assets/js/site.js
@@ -6,6 +6,8 @@ if ("serviceWorker" in navigator) {
 }
 
 
+let activePage=document.getElementById('drawPage');
+
 
 // add draw engine to html id
 
@@ -29,10 +31,75 @@ clearCache.addEventListener('click', () => {
 
 
 
+
 export function LoadSlideCallback(myIndex) {
 
     drawEngine.LoadSlide(LoadSlideImage(myIndex))
 }
 
+// menu bar  create dynamic menu later
+
+let myShotListbutton = document.getElementById('shotList');
+myShotListbutton.setAttribute("data-page", 'shotlist');
+
+myShotListbutton.addEventListener('click',menuCallback)
+
+let myLoggingButton = document.getElementById('logging');
+myLoggingButton.setAttribute("data-page", 'logging');
+
+myLoggingButton.addEventListener('click',menuCallback)
+
+
+let myDrawButton = document.getElementById('drawButton');
+myDrawButton.setAttribute("data-page",'draw');
+
+myDrawButton.addEventListener('click',menuCallback)
+
+
+function menuCallback(e){
+
+    activePage.classList.toggle('hidden');
+
+
+    let myDrawPage=document.getElementById('drawPage');
+    let myShotlistPage=document.getElementById('shotListPage');
+    let myLoggingPage=document.getElementById('loggingPage');
+
+
+let myPage=e.target.dataset.page;
+
+console.log(myPage);
+
+
+switch(myPage){
+
+    case 'shotlist':
+    //load shotlist page
+    myShotlistPage.classList.toggle('hidden');
+    activePage=myShotlistPage
+   
+
+    break;
+
+    case 'logging':
+        //load shotlist page
+  
+    myLoggingPage.classList.toggle('hidden');
+    activePage=myLoggingPage
+        break;
+
+        case 'draw':
+            //load shotlist page
+            myDrawPage.classList.toggle('hidden');
+            activePage=myDrawPage
+
+            break;
+
+  default:
+    //do nothing
+    break;
+}
+
+}
 
 
